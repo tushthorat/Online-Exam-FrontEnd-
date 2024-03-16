a232edb385520938978b5fb48ee8a9865679e58f
tushthorat <thoratt49@gmail.com>
2024-03-16 17:36:33 +0530

    commit

--- src/app/combobox/combobox.component.ts
+++ src/app/combobox/combobox.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-combobox',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './combobox.component.html',
   styleUrl: './combobox.component.css'
 })
-export class ComboboxComponent {
+export class ComboboxComponent implements OnInit {
+  constructor(private router:Router)
+  {
 
+  }
+
+  ngOnInit(): void 
+  {
+    let username=sessionStorage.getItem('username');
+    console.log(username);
+
+    if(username==null)
+    {
+      this.router.navigate(['login']);
+    }
+  }
+
+  selectlanguage:string=""; // it will get selected option value
+
+  display()
+  {
+    console.log(' selected language is ' + this.selectlanguage);
+  }
 }
